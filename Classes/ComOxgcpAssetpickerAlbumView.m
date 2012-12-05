2e6d85328b81af66fd67e07683aca0a084ffe49f
metacortex <9ruvie@gmail.com>
2012-12-06 00:46:49 +0900

    selected album returns numberOfAssets

--- Classes/ComOxgcpAssetpickerAlbumView.m
+++ Classes/ComOxgcpAssetpickerAlbumView.m
@@ -111,7 +111,7 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     ALAssetsGroup *group = [groups objectAtIndex:indexPath.row];
-    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:[group valueForProperty:ALAssetsGroupPropertyName], @"groupName", nil];
+    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:[group valueForProperty:ALAssetsGroupPropertyName], @"groupName", [group numberOfAssets], @"numberOfAssets", nil];
     
     [self.proxy fireEvent:@"album:selected" withObject:dic];
 }
